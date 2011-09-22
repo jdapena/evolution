0feb111aa9406d18288382412ad65be888b74539
Dan Vrátil <dvratil@redhat.com>
2011-09-22 15:25:23 +0200

    Add attachments to store during parsing, not during writing

--- em-format/em-format.c
+++ em-format/em-format.c
@@ -77,7 +77,6 @@ static void emf_parse_message			(EMFormat *emf, CamelMimePart *part, GString *pa
 static void emf_parse_headers			(EMFormat *emf, CamelMimePart *part, GString *part_id, EMFormatParserInfo *info, GCancellable *cancellable);
 static void emf_parse_post_headers		(EMFormat *emf, CamelMimePart *part, GString *part_id, EMFormatParserInfo *info, GCancellable *cancellable);
 static void emf_parse_source			(EMFormat *emf, CamelMimePart *part, GString *part_id, EMFormatParserInfo *info, GCancellable *cancellable);
-static void emf_parse_attachment		(EMFormat *emf, CamelMimePart *part, GString *part_id, EMFormatParserInfo *info, GCancellable *cancellable);
 
 /* WRITERS */
 static void emf_write_text			(EMFormat *emf, EMFormatPURI *puri, CamelStream *stream, GCancellable *cancellable) {};
@@ -1062,21 +1061,6 @@ emf_parse_source (EMFormat *emf,
 	em_format_add_puri (emf, puri);
 }
 
-static void
-emf_parse_attachment (EMFormat *emf,
-		      CamelMimePart *part,
-		      GString *part_id,
-		      EMFormatParserInfo *info,
-		      GCancellable *cancellable)
-{
-	EMFormatPURI *puri;
-
-	puri = em_format_puri_new (emf, sizeof (EMFormatPURI), part, part_id->str);
-	puri->is_attachment = TRUE;
-
-	em_format_add_puri (emf, puri);
-}
-
 /**************************************************************************/
 
 void
@@ -1209,7 +1193,6 @@ static EMFormatHandler type_handlers[] = {
 		{ (gchar *) "x-evolution/message/headers", emf_parse_headers, },
 		{ (gchar *) "x-evolution/message/post-headers", emf_parse_post_headers, },
 		{ (gchar *) "x-evolution/message/source", emf_parse_source, },
-		{ (gchar *) "x-evolution/message/attachment", emf_parse_attachment, },
 };
 
 /* note: also copied in em-mailer-prefs.c */
